c0768391787bd85165617662922e58ef65cd8f34
zanderson1998 <zanderson1998@yahoo.com>
2020-12-01 17:01:46 -0700

    Add drag and drop capabilities

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,55 @@
 var tasks = {};
 
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event){
+    console.log("over", event.target);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event){
+    console.log("over", event.target);
+  },
+  out: function(event){
+    console.log("out", event.target);
+  },
+  update: function(event) {
+    //array to store the task data in
+    var tempArr = [];
+
+    //loop over current set of children in sortable list
+    $(this).children().each(function() {
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim()
+
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim()
+
+      //add task to the temp array as an object
+      tempArr.push({
+        text: text,
+        date: date
+      });
+    });
+    //trim down list's ID to match object property
+    var arrName = $(this)
+      .attr("id")
+      .replace("list-","");
+
+    //update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+  }
+});
+
 var createTask = function(taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $("<li>").addClass("list-group-item");
